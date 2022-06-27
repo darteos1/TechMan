e1804680e803426068c08fe557fc6ed5e2b1d67d
LucasSouza <82621071+LucasSouza2021@users.noreply.github.com>
2022-06-27 15:20:51 -0300

    Notificação de ERRO no login/senha

--- techman_project/resources/views/auth/login.blade.php
+++ techman_project/resources/views/auth/login.blade.php
@@ -52,8 +52,7 @@
                                 @enderror
                                 <a id="ver-senha" onclick="mostrarSenha()">👁‍🗨</a>
                             </div>
-                            
-                            
+                                                        
                             <button class="button" id="botao">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z" />
@@ -61,16 +60,6 @@
                             </button>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
                     </form>
                 </div>
             </div>
