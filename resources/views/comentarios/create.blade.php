107bfbbc3344b31ae502e0a300a9f4ca41cfbd55
SilvioMVieira <silvio.vieira@ba.estudante.senai.br>
2022-06-29 20:55:06 -0300

    cadastro comentário

--- resources/views/comentarios/create.blade.php
+++ resources/views/comentarios/create.blade.php
@@ -3,64 +3,62 @@
 @section('title', 'Novo Comentário')
 
 @section('content')
-<div class="container-fluid">
+<div class="container-sm bg-success" style="width: 600px;">
     <h3 id="comentario">Cadastrar Comentário</h3>
-</div>
 
-<form class="form-control border-0" action="{{ route('comentarios-store') }}" method="POST">
-    @csrf
-    <div class="form-floating">
-        <input name="permissao" type="text" class="form-control" id="floatingInput" value={{ auth()->user()->permissao }} hidden><br>
-    </div>
-    <div class="form-floating">
-        <input name="data" type="date" class="form-control" id="floatingInput" value={{ now() }} hidden>
-    </div>
-    <div class="form-floating">
-        <textarea name="comentario" id="comentario" cols="50" rows="5" class="form-control" oninput="toggleButtom()"></textarea>
-        <label for="floatingInput">Comentário: </label>
-    </div>
-    <div class="form-floating">
-        @foreach($equipamentos as $equipamento)
-        <input name="id_equipamento" type="text" class="form-control" id="floatingInput" value="{{ $equipamento->id }}" hidden><br>
-        @endforeach
-    </div>
-    <div class="container mt-1">
-        <div class="form-group border-0 ">
-            <div class="row border border-0">
-                <div type="button" class="col-md-10 ms-3 bg-light position-relative" data-bs-toggle="modal" data-bs-target="#mensagem">
-                    <input type="submit" class="btn btn-primary m-2 text-light position-absolute top-50 start-50 translate-middle" name="submit" id="cadastrar" value="Cadastrar" disabled>
+    <form class="form-control border-0 mx-auto" action="{{ route('comentarios-store') }}" method="POST" style="width: 400px;">
+        @csrf
+        <div class="form-floating">
+            <input name="permissao" type="text" class="form-control" id="floatingInput" value={{ auth()->user()->permissao }} hidden><br>
+        </div>
+        <div class="form-floating">
+            <input name="data" type="date" class="form-control" id="floatingInput" value={{ now() }} hidden>
+        </div>
+        <div class="form-floating">
+            <textarea name="comentario" id="comentario" cols="50" rows="5" class="form-control" oninput="toggleButtom()"></textarea>
+            <label for="floatingInput">Comentário: </label>
+        </div>
+        <div class="form-floating">
+            @foreach($equipamentos as $equipamento)
+            <input name="id_equipamento" type="text" class="form-control" id="floatingInput" value="{{ $equipamento->id }}" hidden><br>
+            @endforeach
+        </div>
+        <div class="container mt-1">
+            <div class="form-group border-0 ">
+                <div class="row border border-0">
+                    <div type="button" class="col-md-10 ms-3 bg-light position-relative" data-bs-toggle="modal" data-bs-target="#mensagem">
+                        <input type="submit" class="btn btn-primary m-2 text-light position-absolute top-50 start-50 translate-middle" name="submit" id="cadastrar" value="Cadastrar" disabled>
 
-                </div>
-                <div class="position-relative mx-auto ms-2">
-                    <a href="{{ route('home')}}" class="btn btn-success m-2 text-light position-absolute top-50 start-50 translate-middle">Voltar</a>
-                </div>
-                <div class="modal fade" id="mensagem" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="container-fluid">
-                                    <div class="row">
-                                        <p><strong>Sucesso! Comentário cadastrado para o equipamento.</strong> </p>
+                    </div>
+                    <div class="position-relative mx-auto ms-2">
+                        <a href="{{ route('home')}}" class="btn btn-success m-2 text-light position-absolute top-50 start-50 translate-middle">Voltar</a>
+                    </div>
+                    <div class="modal fade" id="mensagem" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="container-fluid">
+                                        <div class="row">
+                                            <p><strong>Sucesso! Comentário cadastrado para o equipamento.</strong> </p>
 
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="modal-footer">
-                                <div class="col-sm-12 d-grid gap-2 d-md-flex justify-content-md-start">
+                                <div class="modal-footer">
+                                    <div class="col-sm-12 d-grid gap-2 d-md-flex justify-content-md-start">
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
-
         </div>
-    </div>
-</form>
+    </form>
+</div>
 
 <script>
     function toggleButtom() {
